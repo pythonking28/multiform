656f3760c21b5cbe51d744f73861d9dd7201bbea
pythonking28 <nirajthakur2828@gmail.com>
2024-12-09 22:24:50 +0545

    vercel images not loading issue resolution 2

--- src/components/LayoutCard.jsx
+++ src/components/LayoutCard.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const branches = [
   {
     country: "Singapore",
-    image: "public/singapore.png",
+    image: "/singapore.png",
     type: "Head Office",
     company: "XYZ Pvt. Ltd.",
     address: "Road to nowhere, 06-404, 500 Internal Error",
@@ -13,7 +13,7 @@ const branches = [
   },
   {
     country: "Hong Kong",
-    image: "public/hongkong.png",
+    image: "/hongkong.png",
     type: "Branches",
     company: "XYZ Pte. Ltd.",
     address: "The Infinite Loop Office, 404 Timeout Plaza",
@@ -21,7 +21,7 @@ const branches = [
   },
   {
     country: "USA",
-    image: "public/united.png",
+    image: "/united.png",
     type: "Branches",
     company: "XYZ Inc.",
     address: "Nulltown, Buglandia, 500 0xDEADBEE",
